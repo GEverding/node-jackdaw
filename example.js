3d36337861923fdd1511e08e2e7e63ffecbe0d91
Garrett Everding <garrett.everding92@gmail.com>
2013-08-20 13:30:28 -0400

    updated example script

--- example.js
+++ example.js
@@ -4,15 +4,12 @@ var jd = require('./lib/index');
 var dsn = process.env.SENTRY_DSN;
 var client = new jd.Client(dsn);
 
-try {
-  (function someFunction(){
-    throw new Error("ahh something went wrong");
-  })();
-} catch (e) {
-  jd.Event.fromError(e, function(err, event){
-    console.log(client);
-    client.send(event, function(serr, resp, body) {
-      console.log(serr, resp.statusCode, body);
-    });
+process.on('uncaughtException', function(err){
+  client.captureError(err, function(err) {
+    console.log(err)
   });
-}
+});
+
+(function someFunction(){
+  throw new Error('ahh something went wrong');
+})();
